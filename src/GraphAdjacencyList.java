2148fd4bf8bbca1025da50b5c61a6173249aad6b
Jigar Patel <jigarbjpatle@gmail.com>
2013-12-23 04:15:19 -0500

    Graph AdjacencyMatrix

---
+++ src/GraphAdjacencyList.java
@@ -0,0 +1,58 @@
+package edu.cmu.jjpatel;
+
+import java.util.*;
+//Directed Graph
+public class GraphAdjacencyList{
+
+	private LinkedList<Integer>[] internalArray;
+	private int vertices;
+	private int edges;
+
+	public GraphAdjacencyList(int numberOfVertices){
+		this.vertices = numberOfVertices;
+		internalArray = (LinkedList<Integer>[]) new LinkedList[vertices];
+		edges = 0;
+	}
+	public String toString(){
+		StringBuffer sb = new StringBuffer();
+		for(int i=0; i<internalArray.length; i++){
+			LinkedList<Integer> l = internalArray[i];
+			if(l != null){
+				for(Integer j : l)
+					sb.append("Edge " + i + "-" + j + "\n");
+			}
+		}
+		return sb.toString();
+	}
+	public void addEdge(Integer node1, Integer node2){
+		if(node1 >= vertices || node2 >= vertices || node1 < 0 || node2 < 0)
+			throw new IndexOutOfBoundsException("No such vertex");
+		if(internalArray[node1] == null)
+			internalArray[node1] = new LinkedList<Integer>();
+
+		for(Integer i : internalArray[node1]){
+			if(i == node2)
+				return; //edge already exists
+		}
+		internalArray[node1].add(node2);
+		edges++;
+	}
+	public void removeEdge(Integer node1, Integer node2){
+		if(node1 >= vertices || node2 >= vertices || node1 < 0 || node2 < 0)
+			throw new IndexOutOfBoundsException("No such vertex");
+		LinkedList<Integer> l = internalArray[node1];
+		if(l != null){
+			if(l.remove(node2))
+				edges--;
+		}
+
+	}
+	public static void main(String args[]){
+		GraphAdjacencyList g = new GraphAdjacencyList(3);
+		g.addEdge(1,2);
+		g.addEdge(0,1);
+		System.out.println(g);
+		g.removeEdge(0,1);
+		System.out.println(g);
+	}
+}
