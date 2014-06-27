aa4aea5cae4e52049d322d8cc8833bfa6c3f4bf5
Jigar <jigarbjpatel@gmail.com>
2014-06-26 23:39:15 -0700

    Clean up:

---
+++ src/pblms/PowerOutage.java
@@ -0,0 +1,20 @@
+package pblms;
+
+public class PowerOutage {
+
+	public static void main(String[] args) {
+		PowerOutage p = new PowerOutage();
+		int[] from = {0};
+		int[] to = {1};
+		int[] cost = {10};
+		System.out.println(p.estimateTimeOut(from, to, cost));
+	}
+	
+	public int estimateTimeOut(int[] fromJunction, int[] toJunction, int[] ductLength){
+		int minTime = 0;
+		
+		
+		return minTime;
+	}
+
+}
