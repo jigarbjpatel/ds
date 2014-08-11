1278715bcbf283853200f4ea65a7d905cd55b95a
Jigar Patel <jigarbjpatle@gmail.com>
2014-08-10 19:45:30 -0700

    Some more basics

--- src/common/Basics.java
+++ src/common/Basics.java
@@ -1,31 +1,32 @@
-package common;
-public class Basics{
-	static Integer s;
-	final Integer x;
-	public static void main(String args[]){
-		Basics b = new Basics(8);
-		Basics b1 = new Basics(10);
-		System.out.println(b.x + " " + b1.x);
-		System.out.println(System.currentTimeMillis());
-		
+public class Basics implements Comparable<Basics>{
+
+Integer id;
+@Override
+public int compareTo(Basics other){
+	return id.compareTo(other.id);
+}
+	public static void main(String[] args) throws Exception{
+		Integer a =0;
+		Byte b=0;
+		System.out.println(a.equals(b));
+		//System.out.println(m(Integer.parseInt(args[0])));
 	}
-	public Basics(int i){
-		x=i;
-	}
-	static{
-		s = 1;
-	}
-	@Override
-	public int hashCode(){
-		return x.hashCode();	
-	}
-	@Override
-	public boolean equals(Object other){
-		if(other == this)
-			return true;
-		if(other == null)
-			return false;
-		Basics o = (Basics)other;
-		return this.x == o.x;
+	static String m(int a) throws Exception{
+		try{
+			System.out.println("try....");
+			if(a/100 == 2) return "return from try";
+			if(a/100 == 3) throw new Exception("thrown by try");
+			if(a/100 == 4) throw new RuntimeException("thrown by try");
+		}catch(RuntimeException x){
+			System.out.println("catch");	
+			if(a/10%10 == 2) return "returned from catch";
+			if(a/10%10 == 3) throw new Exception("thrown by catch");
+		}finally{
+			System.out.println("finally");
+			if(a%10 == 2) return "returned from finally";
+			if(a%10 == 3) throw new Exception("thrown by finally");
+		}
+		return "terminated normally with " + a;
+
 	}
 }
