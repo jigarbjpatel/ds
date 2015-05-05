1bd13c3f9256cf7b5f76122909db7d5c4b7f1ea3
Jigar <jigarbjpatel@gmail.com>
2015-05-04 19:49:12 -0700

    Update GraphAdjacencyList.java

--- src/GraphAdjacencyList.java
+++ src/GraphAdjacencyList.java
@@ -13,8 +13,9 @@ public class GraphAdjacencyList{
 		internalArray = (LinkedList<Integer>[]) new LinkedList[vertices];
 		edges = 0;
 	}
+	@Override
 	public String toString(){
-		StringBuffer sb = new StringBuffer();
+		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<internalArray.length; i++){
 			LinkedList<Integer> l = internalArray[i];
 			if(l != null){
