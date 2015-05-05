ad3a82bfe31cba8dde9d7d274155fbecfa906028
Jigar <jigarbjpatel@gmail.com>
2015-05-04 20:18:34 -0700

    Update BasicSort.java

--- src/BasicSort.java
+++ src/BasicSort.java
@@ -5,10 +5,11 @@ public class BasicSort{
 	//Total O(n) swaps only but O(n^2) comparisions
 	public void selectionSort(Integer[] data){
 		int minIndex = 0;
+		int n = data.length;
 		//last element is already in its place after n-1 passes
-		for(int i=0; i<data.length-1; i++){
+		for(int i=0; i < n-1; i++){
 			minIndex = i;
-			for(int j=i+1; j<data.length; j++){
+			for(int j=i+1; j < n; j++){
 				if(data[j] < data[minIndex])
 					minIndex = j;
 			}
@@ -20,9 +21,10 @@ public class BasicSort{
 	//O(n^2) in worst case but in place. O(n) if the array is nearly sorted (only when we use a flag)
 	public void bubbleSort(Integer[] data){
 		boolean swapped = false; 
-		for(int i=0; i<data.length; i++){
-			//total N-i-1 comparisions in innerloop 
-			for(int j=0; j<data.length-i-1; j++){
+		int n = data.length;
+		for(int i=0; i < n; i++){
+			//total n-i-1 comparisions in innerloop 
+			for(int j=0; j < n-i-1; j++){
 				if(data[j] > data[j+1]){
 					swap(data,j,j+1); 
 					swapped = true;
@@ -37,22 +39,25 @@ public class BasicSort{
 	//Good for small data and partially sorted data almost O(n)
 	//Also a stable sort 
 	public void insertionSort(Integer[] data){
-		int j;		
-		for(int i=1; i<data.length; i++){
+		int j;
+		int n = data.length;
+		for(int i=1; i < n; i++){
 			Integer currentValue = data[i];
+			
 			for(j=i-1; (j>=0) && (currentValue < data[j]); j=j-1)
 				data[j+1] = data[j];//shift to right
+			
 			data[j+1] = currentValue; //place the current value in correct place relative to previous values
 		}
 	}
 	//h-sort - sort at intervals defined by gap
 	//Used in embedded systems can be O(n^4/3) for particular gap sequence othereise O(n^2)
 	public void shellSort(Integer[] data){
-		int N = data.length;
-		for(int gap=N/2; gap>0; gap=gap/2){
+		int n = data.length;
+		for(int gap = n/2; gap > 0; gap = gap/2){
 			//Do an insertion sort
 			int j;
-			for(int i=gap; i<N; i++){
+			for(int i = gap; i < n; i++){
 				Integer currentValue = data[i];
 				for(j=i-gap; j>=0 && currentValue < data[j]; j=j-gap)
 					data[j+gap] = data[j];
