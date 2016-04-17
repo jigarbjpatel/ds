652b0fba300b48cf506b8cfc2c534013c1bcef9c
Jigar <jigarbjpatel@gmail.com>
2016-04-17 13:09:41 -0700

    Update HeapSort.java

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,30 +1,27 @@
 package edu.cmu.jjpatel;
 
 public class HeapSort{
+	//O(nlogn) time and O(n) space for heap
 	public static void sortiUsingMinHeap(Integer[] data){
 		MinHeap minh = new MinHeap();
 		for(int i=0; i<data.length; i++)
-			minh.insert(data[i]);
+			minh.insert(data[i]); //O(log n)
 
 		for(int i=0; i<data.length; i++)
 			System.out.print(minh.deleteMin());
-	}	
+	}
+	//O(nlogn) but no extra space
 	public  void sort(Integer[] data){
-		buildHeap(data);
-/*			for(int i=0; i<data.length; i++)
-				System.out.print(data[i] + " "); 
-			System.out.println();*/
+		buildHeap(data); //O(n)
 		
 		int N = data.length;
 		while(N > 0){
 			swap(data,0,N-1);
 			N--;
-			percolateDown(data,0,N);
-		/*	for(int i=0; i<data.length; i++)
-				System.out.print(data[i] + " "); 
-			System.out.println();*/
+			percolateDown(data,0,N); //O(logn)
 		}
 	}
+	//We only precolate down half of elements. Why?
 	private void buildHeap(Integer[] data){
 		int N = data.length;
 		for(int i=N/2; i>=0; i--)
@@ -34,15 +31,17 @@ public class HeapSort{
 		int leftChildIndex = 2*index+1;
 		while(leftChildIndex < N){
 			int childToCompareWith = leftChildIndex;
-			if(leftChildIndex < N-1 && data[leftChildIndex] < data[leftChildIndex+1])
+			if(leftChildIndex < N-1 && data[leftChildIndex] < data[leftChildIndex+1]){
 				childToCompareWith = leftChildIndex+1;
+			}
 			if(data[index] < data[childToCompareWith]){
 				swap(data,index,childToCompareWith);
 				index = childToCompareWith;
 				leftChildIndex = 2*index+1;
 			}
-			else
+			else{
 				break;
+			}
 		}
 	}
 	private void swap(Integer[] data, int from, int to){
